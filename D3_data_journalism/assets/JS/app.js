e97ebd098a0842d519f5cddbb009bb7b4d392ff3
abarnatan <ale.barn19@gmail.com>
2021-01-26 21:31:45 -0800

    append g and set params

--- D3_data_journalism/assets/JS/app.js
+++ D3_data_journalism/assets/JS/app.js
@@ -18,3 +18,11 @@ const svg = d3.select("#scatter")
             .append("svg")
             .attr("width", svgWidth)
             .attr("height", svgHeight);
+
+// Append SVG group
+const chartGroup = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+// Set the initial parameters 
+var chosenXaxis = "poverty";
+var chosenYaxis = "healthcare";
