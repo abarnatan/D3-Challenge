ab2260d195b98ac12b7cbc638db2a308c4b71729
abarnatan <ale.barn19@gmail.com>
2021-01-26 21:23:16 -0800

    making svg wrapper

--- D3_data_journalism/assets/JS/app.js
+++ D3_data_journalism/assets/JS/app.js
@@ -7,3 +7,14 @@ const margin = {
     bottom: 80,
     left: 100
 };
+
+// Set width and height of chart
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
+
+// Make SVG wrapper, append an SVG group that will hold our chart,
+// move by left and top margins.
+const svg = d3.select("#scatter")
+            .append("svg")
+            .attr("width", svgWidth)
+            .attr("height", svgHeight);
